d86c9e596d0599969c72a7935f76a6bbbcfa35dc
manishngp <manishngpdoc@gmail.com>
2026-06-06 23:40:52 +0530

    slicing  function in python

---
+++ Strings/str.py
@@ -0,0 +1,12 @@
+name = "Manish Nagpure"
+# Negative indeing to access from last
+print(name[-1]) 
+
+#slicing a string  [start: end(Exculeded): steps]
+print(name[2:4:1]) 
+# gives full string
+print(name[::]) 
+
+
+
+
